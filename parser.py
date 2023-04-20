6f50cf013abf3a87ea6c9bd7913cd5540dc6a365
Stolyarov Max <Windmill78max@gmail.com>
2023-04-21 01:57:50 +0300

    New message await for list of channels

--- parser.py
+++ parser.py
@@ -1,13 +1,20 @@
 from telethon import TelegramClient, events, sync
 import os
-from telegram import Update
+from telegram import Update, Bot
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
-client = TelegramClient('session_name', os.getenv('API_ID'), os.getenv('API_HASH'))
+client = TelegramClient('session_name', int(os.getenv('API_ID')), os.getenv('API_HASH'))
 client.start()
 
-myId = client.get_me().id
+my_id = client.get_me().id
 
-application = ApplicationBuilder().token(os.getenv('SAFE_BOT_KEY')).build()
+bot = Bot(token=os.getenv('SAFE_BOT_KEY'))
 
-#application.bot.send_message(chat_id=myId, text='Hello')
+# TODO: set chats ids
+@client.on(events.NewMessage(chats=[my_id]))
+async def send_message(event):
+    # TODO: check matches
+    await bot.send_message(chat_id=my_id, text='Hello')
+
+if __name__ == '__main__':
+    client.run_until_disconnected()
