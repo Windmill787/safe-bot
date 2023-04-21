9c2f2d51d1c04a15fc883b5f8fad7f68e3830b6c
Stolyarov Max <Windmill78max@gmail.com>
2023-04-21 21:19:28 +0300

    Duplicating matches to bot + reply, removed task file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# safe-bot
+# Parser telegram bot
+# Tracks multiple channels messages then looks for matches and duplicates message to bot
+# No more ads, spam, service messages
--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telethon import TelegramClient, events, sync
 import os
+from telegram import Update, Bot
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import re
+
+client = TelegramClient('session_name', int(os.getenv('API_ID')), os.getenv('API_HASH'))
+client.start()
+
+my_id = client.get_me().id
+channel_matches = []
+channel_ids = []
+for dialog in client.get_dialogs():
+    for match in channel_matches:
+        if re.search(match, dialog.name, re.IGNORECASE):
+            channel_ids.append(dialog.message.peer_id.channel_id)
+
+bot = Bot(token=os.getenv('BOT_KEY'))
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Success')
+@client.on(events.NewMessage(chats=channel_ids))
+async def send_message(event):
+    message_matches = []
+    for message_match in message_matches:
+        if re.search(message_match, event.message.message, re.IGNORECASE):
+            text = event.message.message
+            if event.message.reply_to is not None:
+                reply = await client.get_messages(event.message.peer_id, ids=event.message.reply_to.reply_to_msg_id)
+                text = "Reply to \"%s\": %s" % (reply.message, event.message.message)
+            await bot.send_message(chat_id=my_id, text=text)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(os.getenv('SAFE_BOT_KEY')).build()
-
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-
-    application.run_polling()
+    client.run_until_disconnected()
